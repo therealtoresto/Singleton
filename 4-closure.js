b88c442b2262524c33a0928cf6129be782caaa14
TORESTO <therealtoresto@gmail.com>
2020-06-26 16:59:48 +0300

    functional style Singleton example

---
+++ 4-closure.js
@@ -0,0 +1,11 @@
+'use string';
+
+const singleton = (() => {
+    const instance = {};
+    return () => instance;
+})();
+
+// Usage
+
+console.assert(singleton() === singleton());
+console.log('instances are equal', singleton());
