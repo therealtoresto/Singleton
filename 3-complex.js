b6c49493df21ac4548ec193a83f1da1011c84e92
TORESTO <therealtoresto@gmail.com>
2020-06-26 16:55:34 +0300

    lambda singleton example

---
+++ 3-complex.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Singleton = (() => {
+    let instance;
+
+    function Singleton() {
+        if (instance) return instance;
+        instance = this;
+    }
+
+    Singleton.prototype.test = function() {};
+    
+    return Singleton;
+})();
+
+// Usage
+
+console.assert(new Singleton() === new Singleton());
+console.log('instances are equal');
